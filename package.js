011261eb9d8b673b6190139fa1ab8b2815c87cee
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-26 11:55:45 +0300

    Remove underscore dependency; cleanup whitespace

--- package.js
+++ package.js
@@ -1,16 +1,15 @@
 Package.describe({
-	summary: "Methods to create admin role and add first admin user.",
-  version: "0.1.0",
-	name: "brylie:first-admin",
+  summary: "Methods to create admin role and add first admin user.",
+  version: "0.1.1",
+  name: "brylie:first-admin",
   git: "https://github.com/brylie/meteor-first-user-admin"
 });
 
 Package.on_use(function (api, where) {
   api.versionsFrom("1.0.1");
-	api.use('accounts-base', ['server']);
-	api.use("alanning:roles@1.2.11", ['server']);
-	api.use("underscore", ['server']);
+  api.use('accounts-base', ['server']);
+  api.use("alanning:roles@1.2.11", ['server']);
 
-	api.add_files('server/methods/roles.js', ['server']);
-	api.add_files('server/methods/users.js', ['server']);
+  api.add_files('server/methods/roles.js', ['server']);
+  api.add_files('server/methods/users.js', ['server']);
 });
