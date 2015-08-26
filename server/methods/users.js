c79eb92ae5016af234ce6c6a7ceed109f1884d1c
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-08-26 11:54:59 +0300

    Remove underscore usage

--- server/methods/users.js
+++ server/methods/users.js
@@ -7,6 +7,9 @@ Meteor.methods({
     // Set default admin role if none provided
     var adminRoleName = Meteor.settings.adminRoleName || 'admin';
 
+    // Create placeholder variable for user admin check
+    var userIsAdmin;
+
     // Create admin role if not existing
     Meteor.call('createRoleIfUndefined', adminRoleName);
 
@@ -21,11 +24,14 @@ Meteor.methods({
       // Get the user's roles
       var userRoles = Roles.getRolesForUser(user);
 
-      // Check if current user is admin
-      var userIsAdmin = _.contains(userRoles, adminRoleName);
+      // Check if user roles contains admin role name
+      var adminRoleIndex = userRoles.indexOf(adminRoleName);
+
+      // Convert admin role index to boolean
+      userIsAdmin = (adminRoleIndex >= 0) ? true : false;
 
       // If user is not admin
-      if (!userIsAdmin) {
+      if (userIsAdmin === false) {
         // Add user to admin role
         Roles.addUsersToRoles(userId, [adminRoleName]);
         console.log("Added first user to ", adminRoleName, " role.");
